0250c84e2e3e20cf753a9137012e72830875ee49
Yoshi_0x <yoshihyoda@gmail.com>
2023-12-14 15:28:46 -0800

    init

---
+++ app/hello/page.tsx
@@ -0,0 +1,7 @@
+export default function Hello() {
+  return (
+    <main className="h-screen flex items-center justify-center">
+      <h1 className="text-red-500 text-3xl">Hello, World 🚀🚀🚀🚀🚀🚀🚀</h1>
+    </main>
+  );
+}
