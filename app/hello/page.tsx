a6061f2d98b8acc48b1e72e991294ae62dea2b54
Yoshi_0x <yoshihyoda@gmail.com>
2023-12-14 16:03:54 -0800

    connected homepage with hellopage

--- app/hello/page.tsx
+++ app/hello/page.tsx
@@ -1,7 +1,14 @@
-export default function Hello() {
+import Link from "next/link";
+
+export default function HelloPage() {
   return (
-    <main className="h-screen flex items-center justify-center">
-      <h1 className="text-red-500 text-3xl">Hello, World 🚀🚀🚀🚀🚀🚀🚀</h1>
+    <main className="h-screen flex flex-col items-center justify-center space-y-10">
+      <h1 className="text-red-500 text-4xl font-bold">
+        Hello, World 🚀🚀🚀🚀🚀🚀🚀
+      </h1>
+      <Link className="text-white text-2xl underline" href="/">
+        Home page
+      </Link>
     </main>
   );
 }
